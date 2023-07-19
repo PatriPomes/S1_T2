0e4f41744b8ca8edeb5b05d5bac7ed1617a7d91d
Patricia Manzanas <pmanzanastomas@gmail.com>
2023-07-19 09:47:36 +0200

    Correcciones realitzadas

--- S1_T2_N1.php
+++ S1_T2_N1.php
@@ -73,54 +73,32 @@ echo "<br>";
 echo "<br>";
 
 //-La suma.
-function suma($X,$Y){
-    $suma = $X+$Y;
-    return $suma;
-}
-echo "El resultat de la suma X+Y es: ".suma($X,$Y);
-/*echo "X+Y= ".$X+$Y;*/
+
+echo "X+Y= ".$X+$Y;
 echo "<br>";
-echo "El resultat de la suma N+M es: ".suma($N,$M);
-/*echo "N+M= ".$N+$M;*/
+echo "N+M= ".$N+$M;
 echo "<br>";
 echo "<br>";
 
 //-La resta.
-function resta($X,$Y){
-    $resta = $X-$Y;
-    return $resta;
-}
-echo "El resultat de la resta X-Y es: ".resta($X,$Y);
-//echo "X-Y= ".$X-$Y;
+
+echo "X-Y= ".$X-$Y;
 echo "<br>";
-echo "El resultat de la resta N-M es: ".resta($N,$M);
-//echo "N-M= ".$N-$M;
+echo "N-M= ".$N-$M;
 echo "<br>";
 echo "<br>";
 
 //-El producte
-function producte($X,$Y){
-    $producte = $X*$Y;
-    return $producte;
-}
-echo "El resultat del producte X*Y es: ".producte($X,$Y);
-//echo "X*Y= ".$X*$Y;
+echo "X*Y= ".$X*$Y;
 echo "<br>";
-echo "El resultat del producte N*M es: ".producte($N,$M);
-//echo "N*M= ".$N*$M;
+echo "N*M= ".$N*$M;
 echo "<br>";
 echo "<br>";
 
 //-El mòdul.
-function modul($X,$Y){
-    $modul = $X%$Y;
-    return $modul;
-}
-echo "El resultat del modul X%Y es: ".modul($X,$Y);
-//echo "X%Y= ".$X%$Y;
+echo "X%Y= ".$X%$Y;
 echo "<br>";
-echo "El resultat del modul N%M es: ".modul($N,$M);
-//echo "N%M= ".$N%$M;
+echo "N%M= ".$N%$M;
 echo "<br>";
 echo "<br>";
 
@@ -153,8 +131,6 @@ echo "<br>";
 // i en un tercer paràmetre et permeti fer la suma, la resta, la multiplicació o la divisió dels dos nombres.
 echo "<b>Ejercicio 3b </b>";
 echo "<br>";
-function calculator(){
-
 $n1=$_POST['Number1'];
 echo "El primer numero elegido es ".$n1;
 echo "<br>";
@@ -162,15 +138,17 @@ $n2=$_POST['Number2'];
 echo "El segundo numero elegido es ".$n2;
 echo "<br>";
 $oper=$_POST['operacion'];
+function calculator($n1,$n2,$oper){
+
 switch ($oper){
     case "suma":
-        $total=suma($n1,$n2);
+        $total=$n1+$n2;
         break;
     case "resta":
-       $total=resta($n1,$n2);
+       $total=$n1-$n2;
         break;
     case "multiplicacion":
-        $total=producte($n1,$n2);
+        $total=$n1*$n2;
         break;
     case "division":
         $total=$n1/$n2;
@@ -180,38 +158,40 @@ switch ($oper){
 }
  return $total;
 }
-echo "El resultat es ".calculator();
+echo "El resultat es ".calculator($n1,$n2,$oper);
 echo "<br>";
 echo "<br>";
 
 //Ejercicio 4:
 echo "<b>Ejercicio 4 </b>";
 echo "<br>";
-function contador(){
-    $limite=$_POST['limite'];
-    $secuencia=$_POST['secuencia'];
+$limite=$_POST['limite'];
+$secuencia=$_POST['secuencia'];
+function contador($limite,$secuencia){
+    define ("predeterminat", 10);
 
    if ($limite==0){
-        for ($con=0;$con<=10;$con+=$secuencia){
+        for ($con=0;$con<=predeterminat;$con+=$secuencia){
            echo  $con.",";
         }
     }else{
-        for ($con=0;$con<=10;$con+=$secuencia){
+        for ($con=0;$con<=$limite;$con+=$secuencia){
             echo $con.",";
         }
     }
     echo "<br>";
     echo "Has elegido que cuente de ".$secuencia." en ".$secuencia;
 }
-contador();
+contador($limite,$secuencia);
 echo "<br>";
 echo "<br>";
 
 //Ejercicio 5:
 echo "<b>Ejercicio 5</b>";
 echo "<br>";
-function grau(){
-    $nota=$_POST['nota'];
+$nota=$_POST['nota'];
+function grau($nota){
+    
     if ($nota>="6"){
         echo "Enorabuena, estas en PRIMERA DIVISION!!!! ";
     }elseif ("5.9">=$nota && $nota>="4.5" ){
@@ -222,13 +202,14 @@ function grau(){
         echo "Tendras que reprobar, lo lamento.";
     }
 }
-grau();
+grau($nota);
 echo "<br>";
 echo "<br>";
 
 //Ejercicio 6:
 echo "<b>Ejercicio 6</b>";
 echo "<br>";
+
  function isBitten(){
     $bitten=rand(0,1);
         if($bitten==0){
